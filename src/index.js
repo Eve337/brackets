c4b6e0c50cb4293f0d10e57bec092731dbcdceb1
eve337 <vesunchik1945@gmail.com>
2021-01-29 12:50:04 +0300

    init: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let arr = str.split("");
+    let halfOfArr = arr.length / 2;
+    for (let j = 0; j < halfOfArr; j++){
+        for(let i = 0; i < arr.length; i++){
+            if (arr[i] == "{" && arr[i + 1] == "}" || arr[i] == "[" && arr[i + 1] == "]" ||
+                arr[i] == "(" && arr[i + 1] == ")" || arr[i] == "|" && arr[i + 1] == "|" ||
+                arr[i] == "1" && arr[i + 1] == "2" || arr[i] == "3" && arr[i + 1] == "4" ||
+                arr[i] == "5" && arr[i + 1] == "6" || arr[i] == "7" && arr[i + 1] == "7" ||
+                arr[i] == "8" && arr[i + 1] == "8"){
+                arr.splice(i,2);
+            } 
+        }
+    }
+    if (arr.length == 0){
+        return true;
+    } else {
+        return false;
+    }
 }
